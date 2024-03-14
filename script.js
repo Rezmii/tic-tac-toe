5a593005f02da001cbc4f3c0dff22b5a392813e8
Rezmii <bartoszrezmer20@gmail.com>
2024-03-14 18:39:14 +0100

    tic tac toe logic and website display of game

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const gameBoardDiv = document.querySelector(".gameboard-div");
+const divElement = document.querySelector(".divElement");
+let currentStatus = "";
 
 function createGameBoard(game) {
   const gameName = game;
@@ -23,7 +25,7 @@ const Player1 = createPlayer("Player1");
 const Player2 = createPlayer("Player2");
 
 displayGameBoard(gameBoard);
-clickOnPositions();
+setupClickHandler();
 
 function displayGameBoard(gameBoard) {
   gameBoardDiv.innerHTML = "";
@@ -38,13 +40,141 @@ function displayGameBoard(gameBoard) {
   });
 }
 
-function clickOnPositions() {
-  let positions = document.querySelectorAll(".position-div");
-  positions.forEach((position, i) => {
-    position.addEventListener("click", () => {
-      gameBoard.gameBoardArr[i] = "X";
-      console.log(gameBoard.gameBoardArr[i]);
+function setupClickHandler() {
+  gameBoardDiv.addEventListener("click", function (event) {
+    if (event.target.classList.contains("position-div")) {
+      const positionIndex = Array.from(gameBoardDiv.children).indexOf(
+        event.target
+      );
+
+      if (
+        gameBoard.gameBoardArr[positionIndex] !== "X" &&
+        gameBoard.gameBoardArr[positionIndex] !== "O"
+      ) {
+        if (currentStatus === "" || currentStatus === "O") {
+          gameBoard.gameBoardArr[positionIndex] = "X";
+          currentStatus = "X";
+        } else if (currentStatus === "X") {
+          gameBoard.gameBoardArr[positionIndex] = "O";
+          currentStatus = "O";
+        }
+      }
       displayGameBoard(gameBoard);
-    });
+      gameRules(gameBoard, currentStatus);
+      console.log(gameBoard);
+    }
+  });
+}
+
+function gameRules(gameBoard, val) {
+  // Warunek dla pierwszego wiersza
+  if (
+    val === gameBoard.gameBoardArr[0] &&
+    val === gameBoard.gameBoardArr[1] &&
+    val === gameBoard.gameBoardArr[2]
+  ) {
+    getWinningPositions(0, 1, 2);
+    displayButton();
+  }
+  // Warunek dla drugiego wiersza
+  else if (
+    val === gameBoard.gameBoardArr[3] &&
+    val === gameBoard.gameBoardArr[4] &&
+    val === gameBoard.gameBoardArr[5]
+  ) {
+    getWinningPositions(3, 4, 5);
+    displayButton();
+  }
+  // Warunek dla trzeciego wiersza
+  else if (
+    val === gameBoard.gameBoardArr[6] &&
+    val === gameBoard.gameBoardArr[7] &&
+    val === gameBoard.gameBoardArr[8]
+  ) {
+    getWinningPositions(6, 7, 8);
+    displayButton();
+  }
+  // Warunek dla pierwszej kolumny
+  else if (
+    val === gameBoard.gameBoardArr[0] &&
+    val === gameBoard.gameBoardArr[3] &&
+    val === gameBoard.gameBoardArr[6]
+  ) {
+    getWinningPositions(0, 3, 6);
+    displayButton();
+  }
+  // Warunek dla drugiej kolumny
+  else if (
+    val === gameBoard.gameBoardArr[1] &&
+    val === gameBoard.gameBoardArr[4] &&
+    val === gameBoard.gameBoardArr[7]
+  ) {
+    getWinningPositions(1, 4, 7);
+    displayButton();
+  }
+  // Warunek dla trzeciej kolumny
+  else if (
+    val === gameBoard.gameBoardArr[2] &&
+    val === gameBoard.gameBoardArr[5] &&
+    val === gameBoard.gameBoardArr[8]
+  ) {
+    getWinningPositions(2, 5, 8);
+    displayButton();
+  }
+  // Warunek dla przekątnej z lewej góry do prawego dołu
+  else if (
+    val === gameBoard.gameBoardArr[0] &&
+    val === gameBoard.gameBoardArr[4] &&
+    val === gameBoard.gameBoardArr[8]
+  ) {
+    getWinningPositions(0, 4, 8);
+    displayButton();
+  }
+  // Warunek dla przekątnej z prawej góry do lewego dołu
+  else if (
+    val === gameBoard.gameBoardArr[2] &&
+    val === gameBoard.gameBoardArr[4] &&
+    val === gameBoard.gameBoardArr[6]
+  ) {
+    getWinningPositions(2, 4, 6);
+    displayButton();
+  } else {
+    let positionsCount = checkIfAllPositionsFilled(gameBoard);
+    if (positionsCount === 9) restartGameBoard(gameBoard);
+  }
+  //displayGameBoard(gameBoard);
+}
+
+function restartGameBoard(gameBoard) {
+  gameBoard.gameBoardArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+}
+
+function checkIfAllPositionsFilled(gameBoard) {
+  let positionsFilledCount = 0;
+  gameBoard.gameBoardArr.forEach((position) => {
+    if (position === "X" || position === "O") positionsFilledCount++;
+  });
+  return positionsFilledCount;
+}
+
+function getWinningPositions(a, b, c) {
+  [a, b, c].forEach((winningPosition) => {
+    let positionDiv = document.querySelector(
+      `.position-div-${winningPosition}`
+    );
+
+    positionDiv.classList.add("dupa");
+  });
+}
+
+function displayButton() {
+  buttonElement = document.createElement("button");
+  buttonText = document.createTextNode("Repeat");
+  buttonElement.appendChild(buttonText);
+  divElement.appendChild(buttonElement);
+  buttonElement.addEventListener("click", () => {
+    restartGameBoard(gameBoard);
+    displayGameBoard(gameBoard);
+    buttonElement.remove();
   });
 }
